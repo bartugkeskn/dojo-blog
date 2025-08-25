f0146820f7b84b44a8bcce717db0d102348426a8
bartugkeskn <bartukeskin1999@gmail.com>
2025-08-25 22:50:07 +0300

    first-commit

---
+++ src/useFetch.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const abortCont = new AbortController();
+
+        fetch(url)
+            .then(res => {
+                if (!res.ok) {
+                    throw Error('Could not fetch the data for that resource');
+                }
+                return res.json();
+            })
+            .then(data => {
+                setData(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
+            });
+
+            return () => { console.log('Cleanup function called'); }  // Optional cleanup}
+    }, [url]);
+
+    return { data, isPending, error };
+}
+
+export default useFetch;
